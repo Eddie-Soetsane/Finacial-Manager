6d1dffc2a0f6e525d0b405b5ce76fdbb06b54e98
Eddie-Soetsane <1918211@brunel.ac.uk>
2023-12-04 07:24:32 +0000

    Models added
    User's can now add models and expenses into the program manually using the admin features found in Django. Next would be to allow users to add projects without being admin and a couple quality of life changes needed.

--- mysite/BudgetTracker/views.py
+++ mysite/BudgetTracker/views.py
@@ -1,8 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Project
 
 def project_list(request):
     return render(request, 'Budget/project-list.html')
 
 def project_detail(request, project_slug):
     #fetching the correct project
-    return render(request, 'Budget/project-detail.html')
+
+    project = get_object_or_404(Project, slug=project_slug)
+    return render(request, 'Budget/project-detail.html', {'project': project, 'expense_list': project.expenses.all()})
